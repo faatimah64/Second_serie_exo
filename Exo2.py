750307222a19a6eb42774a62410e0b20d10d4944
Fatoumata Binta Camara <fatoumacra0@gmail.com>
2022-05-16 16:53:29 +0000

    Utilisation de la boucle while

--- Exo2.py
+++ Exo2.py
@@ -4,6 +4,14 @@ def f(i,j):
     s2=0
     for x in range (i,j+1):
         s2= s2 + x 
+    y=i
+    s3=0
+    while(y<j+1):
+        s3=s3+y
+        y=y+1
+    return s1, s2,s3
+v= f(2,4)
+print(v)
 
 
 
